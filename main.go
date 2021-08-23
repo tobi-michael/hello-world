a7f6c2ca7e0451ec68c26b0acadb3eed64743965
tobi michael <tobimichael65@gmail.com>
2021-08-23 17:05:11 +0100

    modified3

--- main.go
+++ main.go
@@ -6,10 +6,30 @@ func main() {
 	fmt.Println("hello world")
 goland()
 hahaha()
+tmich("tobi", 26 )
+b, t :=tmich("tobi" ,26)
+
+fmt.Println("result:", b , t)
+
+
+
 }
 func goland() {
 	fmt.Println("my name is tobi" )
 }
 func hahaha() {
 	fmt.Println("47")
+
+
+}
+
+
+func tmich(name string, age int) (string, int) {
+	fmt. Println("name is", name)
+	fmt. Println("age is" , age)
+
+	age = age + 10
+
+	return name,age
+
 }
