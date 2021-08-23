53a9fad397b131a5dbfa7b766933ba4e0e81f82e
tobi michael <tobimichael65@gmail.com>
2021-08-23 15:45:23 +0100

    modified2

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-
+goland()
+hahaha()
 }
-func goland()  {
+func goland() {
 	fmt.Println("my name is tobi" )
+}
+func hahaha() {
+	fmt.Println("47")
 }
